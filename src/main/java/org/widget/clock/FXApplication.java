fbe907d76fad26b64d1084d5e012d942724825d8
Nikitinmn42 <nikitin42maxim@gmail.com>
2020-08-09 21:12:39 +0300

    Added logging ScheduledThreadPoolExecutor replaced by ScheduledExecutorService (single thread)

--- src/main/java/org/widget/clock/FXApplication.java
+++ src/main/java/org/widget/clock/FXApplication.java
@@ -16,19 +16,21 @@ public class FXApplication extends Application {
         primaryStage.initStyle(StageStyle.UTILITY); // UTILITY stage doesn't show TaskBar icon
         primaryStage.setOpacity(0); // This stage is invisible
         primaryStage.show();
+        primaryStage.setHeight(0);
+        primaryStage.setWidth(0);
+        primaryStage.setIconified(true);
         // To make window without task bar icon, title and borders use Popup
         Popup clockPopup = new Popup();
         clockPopup.setAnchorLocation(PopupWindow.AnchorLocation.WINDOW_TOP_LEFT);
         clockPopup.getContent().add(root);
         clockPopup.show(primaryStage);
-        clockPopup.setAnchorX(1430);
-        clockPopup.setAnchorY(830);
+        clockPopup.setAnchorX(1380);
+        clockPopup.setAnchorY(825);
         clockPopup.setOnHidden(event -> {
             Platform.exit();
             System.exit(0);
         });
     }
-
 
     public static void main(String[] args) {
         launch(args);
